fad24cb53f40d9b52d701cd278683539371f0ff5
jimmy <jimmy741217@gmail.com>
2018-01-18 23:21:33 +0800

    test

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ import Root from './Root'
 import './css/demo'
 
 // ....可以不需setTimeout,但網路太快會讓 template 的 mask-wrapper 像是白寫了....
-setTimeout(()=> {
+// setTimeout(()=> {
     ReactDOM.render(
         <Root/>,
         document.getElementById("root")
     )    
-}, 600)
+// }, 600)

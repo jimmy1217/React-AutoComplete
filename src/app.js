05f10934f482d098cf078df2827ea7a75a4d4698
jimmy1217 <jimmy741217@gmail.com>
2023-01-11 10:44:51 +0800

    update app.js
    remove setTimeout

--- src/app.js
+++ src/app.js
@@ -4,10 +4,8 @@ import Root from './Root'
 
 // import './css/demo'
 
-// ....可以不需setTimeout,但網路太快會讓 template 的 mask-wrapper 像是白寫了....
-setTimeout(()=> {
-    ReactDOM.render(
-        <Root/>,
-        document.getElementById("root")
-    )    
-}, 600)
+ReactDOM.render(
+    <Root/>,
+    document.getElementById("root")
+)    
+
